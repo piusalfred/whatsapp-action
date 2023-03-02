ab4a78e9ef75dc328dd4114e9b8c5c10f7bcb32a
Pius Alfred <me.pius1102@gmail.com>
2023-03-03 01:56:31 +0300

    init

--- main.go
+++ main.go
@@ -1,60 +1,22 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"fmt"
-	"io"
+	whatsapp "github.com/piusalfred/whatsapp"
+	whttp "github.com/piusalfred/whatsapp/http"
+	errgroup "golang.org/x/sync/errgroup"
+	"net"
 	"net/http"
-	"net/url"
 	"os"
+	"runtime"
 	"strings"
-	"sync"
+	"syscall"
+	"time"
 )
 
 type (
-	Response struct {
-		StatusCode int                 `json:"status_code,omitempty"`
-		Headers    map[string][]string `json:"headers,omitempty"`
-		Message    *ResponseMessage    `json:"message,omitempty"`
-	}
-
-	ResponseMessage struct {
-		Product  string             `json:"messaging_product,omitempty"`
-		Contacts []*ResponseContact `json:"contacts,omitempty"`
-		Messages []*MessageID       `json:"messages,omitempty"`
-	}
-
-
-	ResponseError struct {
-		Code int            `json:"code,omitempty"`
-		Err  *Error `json:"error,omitempty"`
-	}
-
-
-	MessageID struct {
-		ID string `json:"id,omitempty"`
-	}
-
-	ResponseContact struct {
-		Input      string `json:"input"`
-		WhatsappID string `json:"wa_id"`
-	}
-
-	Text struct {
-		PreviewUrl bool   `json:"preview_url,omitempty"`
-		Body       string `json:"body,omitempty"`
-	}
-
-	Message struct {
-		Product       string `json:"messaging_product"`
-		To            string `json:"to"`
-		RecipientType string `json:"recipient_type"`
-		Type          string `json:"type"`
-		PreviewURL    bool   `json:"preview_url,omitempty"`
-		Text          *Text  `json:"text,omitempty"`
-	}
-	Context struct {
+	Inputs struct {
 		BaseURL           string
 		AccessToken       string
 		Version           string
@@ -63,204 +25,134 @@ type (
 		Recipients        []string
 		Message           string
 		PreviewURL        bool
+		IgnoreErrors      bool
 	}
 
-	Client struct {
-		mu                    *sync.Mutex
-		HTTP                      *http.Client
-		BaseURL                   string
-		Version                   string
-		AccessToken               string
-		PhoneNumberID             string
-		WhatsappBusinessAccountID string
+	Response struct {
+		StatusCode  int
+		PhoneNumber string
 	}
-
-	ClientOption func(*Client)
 )
 
-func WithHTTPClient(http *http.Client) ClientOption {
-	return func(client *Client) {
-		client.HTTP = http
-	}
-}
+func Run(ctx context.Context, inputs *Inputs) error {
 
-func WithBaseURL(baseURL string) ClientOption {
-	return func(client *Client) {
-		client.BaseURL = baseURL
+	recipients := inputs.Recipients
+	nOfRecipients := len(recipients)
+	if nOfRecipients == 0 {
+		return fmt.Errorf("no recipients specified")
 	}
-}
-
-func WithVersion(version string) ClientOption {
-	return func(client *Client) {
-		client.Version = version
-	}
-}
-
-func WithAccessToken(accessToken string) ClientOption {
-	return func(client *Client) {
-		client.AccessToken = accessToken
-	}
-}
-
-func WithPhoneNumberID(phoneNumberID string) ClientOption {
-	return func(client *Client) {
-		client.PhoneNumberID = phoneNumberID
-	}
-}
-
-func WithWhatsappBusinessAccountID(whatsappBusinessAccountID string) ClientOption {
-	return func(client *Client) {
-		client.WhatsappBusinessAccountID = whatsappBusinessAccountID
-	}
-}
-
-func NewClient(opts ...ClientOption) *Client {
-	client := &Client{
-		mu:                       &sync.Mutex{},
-		HTTP:                      http.DefaultClient,
-		BaseURL:                   BaseURL,
-		Version:                   "v16.0",
-		AccessToken:               "",
-		PhoneNumberID:             "",
-		WhatsappBusinessAccountID: "",
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConnsPerHost:   runtime.GOMAXPROCS(0) + 1,
 	}
 
-	for _, opt := range opts {
-		opt(client)
+	httpClient := &http.Client{
+		Transport: transport}
+	options := []whatsapp.ClientOption{
+		whatsapp.WithHTTPClient(httpClient),
+		whatsapp.WithBaseURL(inputs.BaseURL),
+		whatsapp.WithAccessToken(inputs.AccessToken),
+		whatsapp.WithVersion(inputs.Version),
+		whatsapp.WithPhoneNumberID(inputs.PhoneNumberID),
+		whatsapp.WithWhatsappBusinessAccountID(inputs.BusinessAccountID),
+	}
+	client := whatsapp.NewClient(options...)
+
+	message := &whatsapp.TextMessage{
+		Message:    inputs.Message,
+		PreviewURL: inputs.PreviewURL,
 	}
 
-	return client
-}
-
-func (c *Client) SetAccessToken(accessToken string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.AccessToken = accessToken
-}
-
-func (c *Client) SetPhoneNumberID(phoneNumberID string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.PhoneNumberID = phoneNumberID
-}
-
-func (c *Client) SetWhatsappBusinessAccountID(whatsappBusinessAccountID string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.WhatsappBusinessAccountID = whatsappBusinessAccountID
-}
-
-type TextMessage struct {
-	Message    string
-	PreviewURL bool
-}
-
-// SendTextMessage sends a text message to a WhatsApp Business Account.
-func (c *Client) SendTextMessage(ctx context.Context, recipient string, message *TextMessage) (*whttp.Response, error) {
-	httpC := c.HTTP
-	request := &SendTextRequest{
-		BaseURL:       c.BaseURL,
-		AccessToken:   c.AccessToken,
-		PhoneNumberID: c.PhoneNumberID,
-		ApiVersion:    c.Version,
-		Recipient:     recipient,
-		Message:       message.Message,
-		PreviewURL:    message.PreviewURL,
-	}
-	resp, err := SendText(ctx, httpC, request)
-	if err != nil {
-		return nil, fmt.Errorf("failed to send text message: %w", err)
-	}
-
-	return resp, nil
-}
-
-func NewRequestWithContext(ctx context.Context, method, requestURL string, params *RequestParams,
-	payload []byte,
-) (*http.Request, error) {
-	var (
-		body io.Reader
-		req  *http.Request
-	)
-	if params.Form != nil {
-		form := url.Values{}
-		for key, value := range params.Form {
-			form.Add(key, value)
+	errChan := make(chan error, 1)
+	responseChan := make(chan *whttp.Response, nOfRecipients)
+	// before exiting, print all responses
+	defer func() {
+		for resp := range responseChan {
+			stdout.Write([]byte(fmt.Sprintf("response: %+v", resp)))
 		}
-		body = strings.NewReader(form.Encode())
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	} else if payload != nil {
-		body = bytes.NewReader(payload)
-	}
+	}()
+	go func() {
+		errChan <- run(ctx, client, inputs.Recipients, message, responseChan)
+	}()
 
-	// Create the HTTP request
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, requestURL, body)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create new request: %w", err)
+	// Wait for all goroutines to finish or for a signal to be received
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case err := <-errChan:
+		return err
 	}
+}
 
-	// Set the request headers
-	if params.Headers != nil {
-		for key, value := range params.Headers {
-			req.Header.Set(key, value)
+func run(ctx context.Context, client *whatsapp.Client, recipients []string,
+	message *whatsapp.TextMessage, responses chan<- *whttp.Response) error {
+	errg, gctx := errgroup.WithContext(ctx)
+	for _, recipient := range recipients {
+		recipient := recipient
+		message := message
+
+		sendf := func(recipient string, message *whatsapp.TextMessage) func() error {
+			return func() error {
+				resp, err := client.SendTextMessage(gctx, recipient, message)
+				if err != nil {
+					return err
+				}
+				responses <- resp
+				return nil
+			}
 		}
+		errg.Go(sendf(recipient, message))
 	}
 
-	// Set the bearer token header
-	if params.Bearer != "" {
-		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", params.Bearer))
-	}
-
-	// Add the query parameters to the request URL
-	if params.Query != nil {
-		query := req.URL.Query()
-		for key, value := range params.Query {
-			query.Add(key, value)
-		}
-		req.URL.RawQuery = query.Encode()
-	}
-
-	return req, nil
-}
-
-// createRequestURL creates a new request url by joining the base url, api version
-// sender id and endpoints.
-// It is called by the NewRequestWithContext function where these details are
-// passed from the RequestParams.
-func createRequestURL(baseURL, apiVersion, senderID string, endpoints ...string) (string, error) {
-	elems := append([]string{apiVersion, senderID}, endpoints...)
-
-	path, err := url.JoinPath(baseURL, elems...)
+	err := errg.Wait()
+	defer close(responses)
 	if err != nil {
-		return "", fmt.Errorf("failed to join url path: %w", err)
+		return err
 	}
 
-	return path, nil
+	return nil
 }
 
-func (c *Client)SendText(ctx context.Context,recipient string, message *Text) (*whttp.Response, error) {
-	httpC := c.HTTP
-	request := &SendTextRequest{
-		BaseURL:       c.BaseURL,
-		AccessToken:   c.AccessToken,
-		PhoneNumberID: c.PhoneNumberID,
-		ApiVersion:    c.Version,
-		Recipient:     recipient,
-		Message:       message.Message,
-		PreviewURL:    message.PreviewURL,
-	}
-	resp, err := SendText(ctx, httpC, request)
+type readfd int
+
+func (r readfd) Read(buf []byte) (int, error) {
+	n, err := syscall.Read(int(r), buf)
 	if err != nil {
-		return nil, fmt.Errorf("failed to send text message: %w", err)
+		return -1, fmt.Errorf("pesakit base io read error: %w", err)
 	}
 
-	return resp, nil
+	return n, nil
 }
 
+type writefd int
+
+func (w writefd) Write(buf []byte) (int, error) {
+	n, err := syscall.Write(int(w), buf)
+	if err != nil {
+		return -1, fmt.Errorf("bedrock/io write error %w", err)
+	}
+
+	return n, nil
 }
+
+const (
+	stdin  = readfd(0)
+	stdout = writefd(1)
+	stderr = writefd(2)
+)
 
 func main() {
-	context := &Context{
+	inputs := &Inputs{
 		BaseURL:           os.Getenv("INPUT_BASE_URL"),
 		AccessToken:       os.Getenv("INPUT_ACCESS_TOKEN"),
 		Version:           os.Getenv("INPUT_VERSION"),
@@ -268,7 +160,16 @@ func main() {
 		BusinessAccountID: os.Getenv("INPUT_BUSINESS_ACCOUNT_ID"),
 		Recipients:        strings.Split(os.Getenv("INPUT_RECIPIENTS"), ","),
 		Message:           os.Getenv("INPUT_MESSAGE"),
-		PreviewURL:        os.Getenv("INPUT_PREVIEW_URL") == "true",
+		PreviewURL:        os.Getenv("INPUT_PREVIEW_URL") == "1",
 	}
-	fmt.Println("Hello, World!")
+
+	ctx := context.Background()
+	nctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	if err := Run(nctx, inputs); err != nil {
+		fmt.Fprintf(stderr, "error: %s", err.Error())
+		os.Exit(1)
+	}
+
 }
